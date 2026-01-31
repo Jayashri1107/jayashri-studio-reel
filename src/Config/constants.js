007f44ec8d651f43fa3e2fe0db851a4f08c46df9
jayashri sable <jsable554@gmail.com>
2026-01-31 12:56:16 +0530

    Update code changes

--- src/Config/constants.js
+++ src/Config/constants.js
@@ -1,4 +1,4 @@
 // Application Constants
-// export const BASE_URL = '' // Development URL (using proxy in package.json)
-export const BASE_URL = 'https://studio-api.ipshopy.com' // Production URL
+export const BASE_URL = '' // Development URL (using proxy in package.json)
+// export const BASE_URL = 'https://studio-api.ipshopy.com' // Production URL
 export const API_BASE_URL = `${BASE_URL}/api`;
